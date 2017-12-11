14e884bc3909d8cc3af683fff238f9b3cf36b70d
Carlos <Carlos@10.34.81.32>
2017-12-11 16:56:35 +0100

    Prueba

---
+++ src/for1.java
@@ -0,0 +1,13 @@
+
+public class for1 {
+
+	public static void main(String[] args) {
+		// inicializacion
+		int count = 1;
+		// Proceso y salida
+		while (count <= 6) {
+			System.out.println("Sleep");
+			count++;
+		}
+	}
+}
